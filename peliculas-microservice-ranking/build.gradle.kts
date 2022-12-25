c4cb2f0330ce2bcf5bc60110fc475c3a7ddefdf5
Fernando Dodino <fernando.dodino@gmail.com>
2022-12-25 02:03:29 -0300

    Getting error

--- peliculas-microservice-ranking/build.gradle.kts
+++ peliculas-microservice-ranking/build.gradle.kts
@@ -1,12 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.0.0"
-	id("io.spring.dependency-management") version "1.1.0"
-	kotlin("jvm") version "1.7.21"
-	kotlin("plugin.spring") version "1.7.21"
-	kotlin("plugin.jpa") version "1.7.21"
-	jacoco
+   id("org.springframework.boot") version "3.0.0"
+   id("io.spring.dependency-management") version "1.1.0"
+   kotlin("jvm") version "1.7.21"
+   kotlin("plugin.spring") version "1.7.21"
+   kotlin("plugin.jpa") version "1.7.21"
+   jacoco
 }
 
 group = "org.uqbar"
@@ -16,85 +16,96 @@ java.sourceCompatibility = JavaVersion.VERSION_17
 extra["springCloudVersion"] = "2022.0.0"
 
 repositories {
-	mavenCentral()
+   mavenCentral()
 }
 
 dependencies {
-	// básicos de cualquier proyecto Spring Boot
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+   // básicos de cualquier proyecto Spring Boot
+   implementation("org.springframework.boot:spring-boot-starter-web")
+   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+   implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+   implementation("org.jetbrains.kotlin:kotlin-reflect")
+   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-	// security
-	implementation("org.springframework.boot:spring-boot-starter-security")
+   // security
+   implementation("org.springframework.boot:spring-boot-starter-security")
 
-	// funciones asincrónicas
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+   // funciones asincrónicas
+   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 
-	// base de datos documental MongoDB
-	implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
+   // base de datos documental MongoDB
+   implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
 
-	// graphql
-	implementation("org.springframework.boot:spring-boot-starter-graphql")
+   // graphql
+   implementation("org.springframework.boot:spring-boot-starter-graphql")
+   implementation("ch.qos.logback:logback-core:1.2.10")
+   implementation("ch.qos.logback:logback-classic:1.2.10")
+   implementation("ch.qos.logback:logback-access:1.2.10")
 
-	// microservicios
-	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client:4.0.0")
+   // microservicios
+   implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client:4.0.0")
 
-	// testing
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("io.projectreactor:reactor-test")
-	testImplementation("org.springframework.graphql:spring-graphql-test")
+   // logging
+   implementation("org.springframework.boot:spring-boot-starter-log4j2")
+   modules {
+      module("org.springframework.boot:spring-boot-starter-logging") {
+         replacedBy("org.springframework.boot:spring-boot-starter-log4j2", "Use Log4j2 instead of Logback")
+      }
+   }
+
+   // testing
+   testImplementation("org.springframework.boot:spring-boot-starter-test")
+   testImplementation("io.projectreactor:reactor-test")
+   testImplementation("org.springframework.graphql:spring-graphql-test")
 }
 
 // microservicios
 dependencyManagement {
-	imports {
-		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
-	}
+   imports {
+      mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+   }
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "17"
-	}
+   kotlinOptions {
+      freeCompilerArgs = listOf("-Xjsr305=strict")
+      jvmTarget = "17"
+   }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+   useJUnitPlatform()
 }
 
 tasks.test {
-	finalizedBy(tasks.jacocoTestReport)
+   finalizedBy(tasks.jacocoTestReport)
 }
 
 tasks.jacocoTestReport {
-	dependsOn(tasks.test)
+   dependsOn(tasks.test)
 }
 
 jacoco {
-	toolVersion = "0.8.8"
+   toolVersion = "0.8.8"
 }
 
 tasks.jacocoTestReport {
-	classDirectories.setFrom(
-		files(classDirectories.files.map {
-			fileTree(it) {
-				exclude("**/config/**", "**/entity/**", "**/*Application*.*", "**/ServletInitializer.*")
-			}
-		})
-	)
-	reports {
-		xml.required.set(true)
-		csv.required.set(false)
-		html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
-	}
+   classDirectories.setFrom(
+      files(classDirectories.files.map {
+         fileTree(it) {
+            exclude("**/config/**", "**/entity/**", "**/*Application*.*", "**/ServletInitializer.*")
+         }
+      })
+   )
+   reports {
+      xml.required.set(true)
+      csv.required.set(false)
+      html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
+   }
 }
 
 tasks.register("runOnGitHub") {
-	dependsOn("jacocoTestReport")
-	group = "custom"
-	description = "$ ./gradlew runOnGitHub # runs on GitHub Action"
+   dependsOn("jacocoTestReport")
+   group = "custom"
+   description = "$ ./gradlew runOnGitHub # runs on GitHub Action"
 }
--- peliculas-microservice-ranking/src/test/kotlin/org/uqbar/peliculasmicroserviceranking/PeliculaControllerTests.kt
+++ peliculas-microservice-ranking/src/test/kotlin/org/uqbar/peliculasmicroserviceranking/PeliculaControllerTests.kt
@@ -3,13 +3,12 @@ package org.uqbar.peliculasmicroserviceranking
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 
 @ActiveProfiles("test")
 @SpringBootTest
-@AutoConfigureMockMvc
+//@AutoConfigureMockMvc
 class PeliculaControllerTests {
 
 //	private val mapper = jacksonObjectMapper().apply {
