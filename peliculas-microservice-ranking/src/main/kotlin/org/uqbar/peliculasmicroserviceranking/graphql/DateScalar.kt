57c101a86fb27e7dda2e5301985b557d5babf17a
Fernando Dodino <fernando.dodino@gmail.com>
2024-02-11 13:36:16 -0300

    removing deprecated definitions

--- peliculas-microservice-ranking/src/main/kotlin/org/uqbar/peliculasmicroserviceranking/graphql/DateScalar.kt
+++ peliculas-microservice-ranking/src/main/kotlin/org/uqbar/peliculasmicroserviceranking/graphql/DateScalar.kt
@@ -1,5 +1,8 @@
 package org.uqbar.peliculasmicroserviceranking.graphql
 
+import graphql.GraphQLContext
+import graphql.execution.CoercedVariables
+import graphql.language.Value
 import graphql.schema.Coercing
 import graphql.schema.CoercingSerializeException
 import graphql.schema.GraphQLScalarType
@@ -8,40 +11,48 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.graphql.execution.RuntimeWiringConfigurer
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 private val defaultFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
 
 @Configuration
 class DateScalarConfiguration {
-   @Bean
-   fun dateScalar(): GraphQLScalarType {
-      return GraphQLScalarType.newScalar()
-         .name("Date")
-         .description("Tipo Date para GraphQL")
-         .coercing(object : Coercing<LocalDate?, String?> {
+    @Bean
+    fun dateScalar(): GraphQLScalarType {
+        return GraphQLScalarType.newScalar()
+            .name("Date")
+            .description("Tipo Date para GraphQL")
+            .coercing(object : Coercing<LocalDate?, String?> {
 
-               @Deprecated("Use serialize with GraphQLContext and Locale")
-               override fun serialize(dataFetcherResult: Any): String {
-                  return (dataFetcherResult as? LocalDate)?.format(defaultFormatter)
-                     ?: throw CoercingSerializeException("El objeto no es de tipo LocalDate: ${dataFetcherResult.javaClass.name}")
-               }
+                override fun serialize(
+                    dataFetcherResult: Any,
+                    graphQLContext: GraphQLContext,
+                    locale: Locale
+                ) =
+                    (dataFetcherResult as? LocalDate)?.format(defaultFormatter)
+                        ?: throw CoercingSerializeException("El objeto no es de tipo LocalDate: ${dataFetcherResult.javaClass.name}")
 
-               @Deprecated("Use parseValue with GraphQLContext and Locale")
-               override fun parseValue(input: Any): LocalDate {
-                  return LocalDate.parse(input.toString(), defaultFormatter)
-               }
+                override fun parseValue(
+                    input: Any,
+                    graphQLContext: GraphQLContext,
+                    locale: Locale
+                ): LocalDate =
+                    LocalDate.parse(input.toString(), defaultFormatter)
 
-               @Deprecated("Use parseLiteral with GraphQLContext and Locale")
-               override fun parseLiteral(input: Any): LocalDate {
-                  return LocalDate.parse(input.toString(), defaultFormatter)
-               }
+                override fun parseLiteral(
+                    input: Value<*>,
+                    variables: CoercedVariables,
+                    graphQLContext: GraphQLContext,
+                    locale: Locale
+                ): LocalDate? =
+                    LocalDate.parse(input.toString(), defaultFormatter)
 
-         }).build()
-   }
+            }).build()
+    }
 
-   @Bean
-   fun configurer(): RuntimeWiringConfigurer {
-      val scalarType: GraphQLScalarType = dateScalar()
-      return RuntimeWiringConfigurer { builder -> builder.scalar(scalarType) }
-   }
+    @Bean
+    fun configurer(): RuntimeWiringConfigurer {
+        val scalarType: GraphQLScalarType = dateScalar()
+        return RuntimeWiringConfigurer { builder -> builder.scalar(scalarType) }
+    }
 }
